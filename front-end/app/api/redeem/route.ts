39d9873d471100476ed1e4a358ad6b60acaf8be2
mathis sergent <sergent.mathis@gmail.com>
2024-11-17 01:40:02 +0700

    Nextjs APIs

--- front-end/app/api/redeem/route.ts
+++ front-end/app/api/redeem/route.ts
@@ -3,6 +3,10 @@ export async function POST(request: Request) {
 
         // TODO Add back-end logic.
 
+        // Example
+        const body = await request.json();
+        const { param1, param2 } = body;
+
         return new Response(JSON.stringify({ name: "Validation Success" }), {
             status: 200,
             headers: {
