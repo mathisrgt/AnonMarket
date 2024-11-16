3fed3f4e23d0a107b3442f23e63ae1dcc1ae730d
vadim91200 <kamenets@hotmail.fr>
2024-11-16 15:26:14 +0700

    feat: :construction: adding deployment to other chain

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -7,13 +7,22 @@ dotenv.config();
 
 const PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "";
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
-
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
   networks: {
     sepolia: {
       url: SEPOLIA_RPC_URL,
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+    rskTestnet: {
+      url: "https://rpc.testnet.rootstock.io/sJNoNnADg2VjGzJZ3dYKaHWbLKr4gs-T",
+      chainId: 31,
+      gasPrice: 60000000,
+      accounts: [PRIVATE_KEY],
+    },
+    zircuit: {
+      url: `https://zircuit1-testnet.p2pify.com`,
+      accounts: [PRIVATE_KEY]
     },
   },
   etherscan: {
